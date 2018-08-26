d7e6d6c7c5a5f42ee6d61fa64bc4d66940233aeb
Alex <aleksandrs.valenieks@gmail.com>
2018-08-26 10:35:48 +0300

    7 домашка

--- 7/7-1/js/script.js
+++ 7/7-1/js/script.js
@@ -5,7 +5,7 @@ let tab = document.getElementsByClassName('info-header-tab'),
 	info = document.getElementsByClassName('info-header')[0];
 
 	function hideTabContent(a) {
-		for (let i = a; i < tabContent.lenght; i++){
+		for (let i = a; i < tabContent.length; i++){
 			tabContent[i].classList.remove('show');
 			tabContent[i].classList.add('hide');
 		}
@@ -24,7 +24,7 @@ let tab = document.getElementsByClassName('info-header-tab'),
 	info.addEventListener('click', function(event) {
 		let target = event.target;
 		if (target.className == 'info-header-tab') {
-			for (let i = 0; i < tab.lenght; i++) {
+			for (let i = 0; i < tab.length; i++) {
 				if (target == tab[i]) {
 					showTabContent(i);
 					break;
